bfd2cb8158f0ca3f6b292e0184ca12e12bc0b44e
Dana Kleifield <dana@kleifield.com>
2017-08-27 13:59:31 -0500

    Second Programming Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## This function defines the accessor and mutator functions ('get' and 'set')
+## for both a matrix object and its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    invM <- NULL
+    set <- function(y) {
+        x <<- y
+        invM <<- NULL
+    }
+    get <- function() x
+    setInv <- function() invM <<- solve(x)
+    getInv <- function() invM
+    list(set = set, get = get,
+         setInv = setInv,
+         getInv = getInv)
 }
 
-
-## Write a short comment describing this function
+## This function checks for whether a valid result-- the matrix's inverse-- exsits in the cache, and returns it.
+## If not, it inverts the matrix and returns the result, using the functions defined in makeCacheMatrix().
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    invM <- x$getInv()
+    if(!is.null(invM)) {
+        message("getting cached data")
+        return(invM)
+    }
+    data <- x$get()
+    invM <- solve(data, ...)
+    x$setInv()
+    invM
 }
